cfd56244a7029d997db21da4226bdf3df5702935
Filip Szettel <filu3@protonmail.com>
2019-09-28 01:05:07 +0100

    counter add buttons and functionality

--- 9-click_counter/app.js
+++ 9-click_counter/app.js
@@ -4,14 +4,30 @@ class Counter extends React.Component {
         result: 0,
     }
     
+    handleMathClick(type, number){
+        // debugger
+        if(type === 'substraction') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result - number,
+            }))
+        
+        } else if(type === 'reset') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: 0,
+        }))
+        }
+    }
+
     render() {
         return(
             <>
+                <button onClick={this.handleMathClick.bind(this, 'substraction', 10)}>-10</button>
+                <button onClick={() => this.handleMathClick('substraction', 1)}>-1</button>
+                <button onClick={this.handleMathClick.bind(this, 'reset')}>Reset</button>
+                <button onClick={() => this.handleMathClick('addition', 1)}>+1</button>
                 <button>+10</button>
-                <button>+1</button>
-                <button>Reset</button>
-                <button>-1</button>
-                <button>-10</button>
                 <h1>Number of clicks: {this.state.count}</h1>
                 <h1>Score: {this.state.result}</h1>
             </>
