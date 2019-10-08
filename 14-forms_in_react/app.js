756f8fb2dc69643c7cec9e8dbf75d957fc290319
Filip Szettel <filu3@protonmail.com>
2019-10-08 04:44:01 +0100

    forms_in_react init, add forms, add functionality

---
+++ 14-forms_in_react/app.js
@@ -0,0 +1,77 @@
+class Form extends React.Component {
+    state = {
+        city: "London",
+        text: "",
+        isLoved: true,
+        number: "0",
+    }
+
+    handleCityChange = (e) => {
+        this.setState({
+            city: e.target.value,
+        })
+    }
+
+    handleTextChange = e => {
+        this.setState({
+            text: e.target.value,
+        })
+    }
+
+    handleIsLovedChange = e => {
+        this.setState({
+            isLoved: e.target.checked,
+        })
+    }
+
+    handleVisitsNumberChange(e) {
+        this.setState({
+            number: event.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    City: <br/>
+                    <input 
+                        value={this.state.city} 
+                        onChange={this.handleCityChange} 
+                        type="text"/>
+                </label>
+                <br/><br/>
+                <label>
+                    Write something about that city: <br/>
+                    <textarea
+                        value={this.state.text} 
+                        onChange={this.handleTextChange}></textarea>
+                </label>
+                <br/><br/>
+                <label>
+                    Do you like that city? <span></span>
+                    <input
+                        type="checkbox"
+                        checked={this.state.isLoved} 
+                        onChange={this.handleIsLovedChange}/>
+                </label>
+                <br/><br/>
+                <label>
+                    How many times you were there? <br/>
+                    <select 
+                        value={this.state.number}
+                        onChange={this.handleVisitsNumberChange.bind(this)}>
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="more">more</option>
+                    </select>
+                    
+                </label>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Form/>, document.getElementById("root"));
